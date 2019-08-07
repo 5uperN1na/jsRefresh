3557cbb14cdaa88950f113442a077e4ff0382c47
Paula Suarez <Paula.Suarez@freeman.com>
2019-08-07 15:42:34 -0500

    Adding updated files to repo.

---
+++ app.js
@@ -0,0 +1,60 @@
+//JavaScript String Data Type
+
+// Creating variables
+var a = 'Hi there!'; // using single quotes
+var b = "Hi there!"; // using double quotes
+
+// Printing variable values
+document.write(a + "<br>");
+document.write(b);
+
+//JavaScript Number Data Type
+
+// Creating variables
+var a = 25;
+var b = 80.5;
+var c = 4.25e+6;
+var d = 4.25e-6;
+
+// Printing variable values
+document.write(a + "<br>");
+document.write(b + "<br>");
+document.write(c + "<br>");
+document.write(d);
+
+//JavaScript Infinity
+document.write("<br>");
+document.write(15 / 0);
+document.write("<br>");
+document.write(-15 / 0);
+document.write("<br>");
+document.write(15 / -0);
+
+//JavaScript NaN
+
+document.write("<br>");
+document.write("Some text" / 2);
+document.write("<br>");
+document.write("Some text" / 2 + 10);
+document.write("<br>");
+document.write(Math.sqrt(-1));
+
+//JavaScript Boolean Data Type
+
+var isReading = true;   // yes, I'm reading
+var isSleeping = false; // no, I'm not sleeping
+
+// Printing variable values
+document.write("<br>");
+document.write(isReading + "<br>");
+document.write(isSleeping);
+
+//JavaScript Undefined Data Type
+// Creating variables
+document.write("<br>");
+var a;
+var b = "Hello World!"
+
+// Printing variable values
+document.write(a + "<br>");
+document.write(b);
